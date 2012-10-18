93924bf90ab55eae99468f55b0fb84d27e18e96d
Dylan Romeo Wahlstrom <dylanwahlstrom@Dylans-MacBook-Pro-2.local>
2012-10-18 12:37:20 -0400

    can't get style drop down to appear correctly in display data.

---
+++ json.js
@@ -0,0 +1,36 @@
+var json ={
+    "appointment1": {
+        "style": ["Style:", "Color"],
+        "fname": ["First Name:", "Dylan"],
+        "lname": ["Last Name:", "Wahlstrom"],
+        "email": ["Email:", "dylanwah1@fullsail.edu"],
+        "sex": ["Sex:", "Male"],
+        "url": ["Website:", "http://3.bp.blogspot.com"],
+        "date": ["Date:", "12/11/12"],
+        "pain": ["Pain Tolerance:", "10"],
+        "comment": ["Comments:", "I want the tattoo on my left lower forearm."]
+    },
+    "appointment2": {
+        "style": ["Style:", "Outline"],
+        "fname": ["First Name:", "Alexis"],
+        "lname": ["Last Name:", "Fuller"],
+        "email": ["Email:", "smlylex@aol.com"],
+        "sex": ["Sex:", "Female"],
+        "url": ["Website:", "http://th09.deviantart.net"],
+        "date": ["Date:", "04/25/13"],
+        "pain": ["Pain Tolerance:", "2"],
+        "comment": ["Comments:", "I am not sure where I want this tattoo or how big."]
+    },
+     "appointment3": {
+        "style": ["Style:", "Grey"],
+        "fname": ["First Name:", "Ellen"],
+        "lname": ["Last Name:", "Holzhauer"],
+        "email": ["Email:", "artygal18@bellsouth.net"],
+        "sex": ["Sex:", "Female"],
+        "url": ["Website:", "http://data.whicdn.com"],
+        "date": ["Date:", "01/14/13"],
+        "pain": ["Pain Tolerance:", "5"],
+        "comment": ["Comments:", "I want the tattoo in the same exact place as the girl in the picture."]
+    }
+    
+}
